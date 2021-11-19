f3983a9f963df2c9f1eb5ed3c2052159e695ee40
Alex Kotov <kotovalexarian@gmail.com>
2021-11-19 10:09:39 +0500

    Fix code style

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,46 @@ use std::ffi::CString;
 use std::thread;
 
 fn main() {
-	unsafe { unsafe_main() }
+    unsafe { unsafe_main() }
 }
 
 unsafe fn unsafe_main() {
-	let wm_pid = libc::fork();
+    let wm_pid = libc::fork();
 
-	if wm_pid == -1 {
-		let msg = CString::new(b"polytree-session: WM fork" as &[u8]).unwrap();
-		libc::perror(msg.as_ptr());
-		libc::exit(libc::EXIT_FAILURE);
-	}
+    if wm_pid == -1 {
+        let msg = CString::new(b"polytree-session: WM fork" as &[u8]).unwrap();
+        libc::perror(msg.as_ptr());
+        libc::exit(libc::EXIT_FAILURE);
+    }
 
-	if wm_pid == 0 {
-		let arg0 = CString::new(b"polytreewm" as &[u8]).unwrap();
-		let args = vec![arg0.as_ptr(), std::ptr::null()];
-		libc::execvp(arg0.as_ptr(), args.as_ptr());
-		let msg = CString::new(b"polytree-session: WM exec" as &[u8]).unwrap();
-		libc::perror(msg.as_ptr());
-		libc::exit(libc::EXIT_FAILURE);
-	}
+    if wm_pid == 0 {
+        let arg0 = CString::new(b"polytreewm" as &[u8]).unwrap();
+        let args = vec![arg0.as_ptr(), std::ptr::null()];
+        libc::execvp(arg0.as_ptr(), args.as_ptr());
+        let msg = CString::new(b"polytree-session: WM exec" as &[u8]).unwrap();
+        libc::perror(msg.as_ptr());
+        libc::exit(libc::EXIT_FAILURE);
+    }
 
-	let status_thread = thread::spawn(move || {
-		let pid = libc::fork();
+    let status_thread = thread::spawn(move || {
+        let pid = libc::fork();
 
-		if pid == -1 { return };
+        if pid == -1 { return };
 
-		if pid == 0 {
-			let arg0 = CString::new(b"slstatus" as &[u8]).unwrap();
-			let args = vec![arg0.as_ptr(), std::ptr::null()];
-			libc::execvp(arg0.as_ptr(), args.as_ptr());
-			let msg = CString::new(b"polytree-session: slstatus exec" as &[u8])
-				.unwrap();
-			libc::perror(msg.as_ptr());
-			libc::exit(libc::EXIT_FAILURE);
-		}
-	});
+        if pid == 0 {
+            let arg0 = CString::new(b"slstatus" as &[u8]).unwrap();
+            let args = vec![arg0.as_ptr(), std::ptr::null()];
+            libc::execvp(arg0.as_ptr(), args.as_ptr());
+            let msg = CString::new(b"polytree-session: slstatus exec" as &[u8]).unwrap();
+            libc::perror(msg.as_ptr());
+            libc::exit(libc::EXIT_FAILURE);
+        }
+    });
 
-	let wm_status: i32 = 0;
-	libc::waitpid(wm_pid, wm_status as *mut i32, 0);
+    let wm_status: i32 = 0;
+    libc::waitpid(wm_pid, wm_status as *mut i32, 0);
 
-	status_thread.join().unwrap();
+    status_thread.join().unwrap();
 
-	libc::exit(libc::WEXITSTATUS(wm_status));
+    libc::exit(libc::WEXITSTATUS(wm_status));
 }
