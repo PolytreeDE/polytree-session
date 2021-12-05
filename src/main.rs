8e398757222339222d7cd70cdd73c77049b62085
Alex Kotov <kotovalexarian@gmail.com>
2021-12-05 21:18:14 +0500

    Run bar

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use crate::task::*;
 fn main() {
     let wm_task = WMTask::start(TaskConfig::new("polytreewm")).unwrap();
     let comp_task = CompTask::start(TaskConfig::new("picom")).unwrap();
+    let bar_task = BarTask::start(TaskConfig::new("polybar")).unwrap();
 
     let wm_task_result = wm_task.wait();
 
     comp_task.terminate();
+    bar_task.terminate();
 
     unsafe { libc::exit(wm_task_result.status()) }
 }
@@ -18,6 +20,9 @@ struct WMTask(TaskInfo);
 
 #[derive(Debug)]
 struct CompTask(TaskInfo);
+
+#[derive(Debug)]
+struct BarTask(TaskInfo);
 
 impl Task for WMTask {
     fn new(info: TaskInfo) -> Self {
@@ -30,6 +35,16 @@ impl Task for WMTask {
 }
 
 impl Task for CompTask {
+    fn new(info: TaskInfo) -> Self {
+        Self(info)
+    }
+
+    fn info(&self) -> &TaskInfo {
+        &self.0
+    }
+}
+
+impl Task for BarTask {
     fn new(info: TaskInfo) -> Self {
         Self(info)
     }
