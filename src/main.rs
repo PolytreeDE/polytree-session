c174fd917f71192519dfadba2ffb34dc329d1a5a
Alex Kotov <kotovalexarian@gmail.com>
2021-12-05 20:56:57 +0500

    Provide default implementations for trait Task

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod task;
 
 use crate::task::*;
 
-use std::ffi::CString;
-
 fn main() {
     let wm_task_config = TaskConfig::new("polytreewm");
     let wm_task = WMTask::start(wm_task_config).unwrap();
@@ -15,31 +13,11 @@ fn main() {
 struct WMTask(TaskInfo);
 
 impl Task for WMTask {
-    fn start(config: TaskConfig) -> Result<Self, String> {
-        unsafe {
-            let pid = libc::fork();
-
-            if pid == -1 {
-                return Err("fork".into());
-            }
-
-            if pid == 0 {
-                let arg0 = CString::new(config.exe().as_bytes()).unwrap();
-                let args = vec![arg0.as_ptr(), std::ptr::null()];
-                libc::execvp(arg0.as_ptr(), args.as_ptr());
-                libc::exit(libc::EXIT_FAILURE);
-            }
-
-            Ok(Self(TaskInfo::new(config, pid)))
-        }
+    fn new(info: TaskInfo) -> Self {
+        Self(info)
     }
 
-    fn wait(self) -> TaskResult {
-        unsafe {
-            let status: i32 = 0;
-            libc::waitpid(self.0.pid(), status as *mut i32, 0);
-            let status = libc::WEXITSTATUS(status);
-            TaskResult::new(self.0, status)
-        }
+    fn info(&self) -> &TaskInfo {
+        &self.0
     }
 }
--- src/task.rs
+++ src/task.rs
@@ -1,17 +1,17 @@
-pub trait Task: Sized {
-    fn start(config: TaskConfig) -> Result<Self, String>;
-    fn wait(self) -> TaskResult;
-}
+use std::ffi::CString;
 
+#[derive(Clone)]
 pub struct TaskConfig {
     exe: String,
 }
 
+#[derive(Clone)]
 pub struct TaskInfo {
     config: TaskConfig,
     pid: libc::pid_t,
 }
 
+#[derive(Clone)]
 pub struct TaskResult {
     info: TaskInfo,
     status: i32,
@@ -54,5 +54,38 @@ impl TaskResult {
 
     pub fn status(&self) -> i32 {
         self.status
+    }
+}
+
+pub trait Task: Sized {
+    fn new(info: TaskInfo) -> Self;
+    fn info(&self) -> &TaskInfo;
+
+    fn start(config: TaskConfig) -> Result<Self, String> {
+        unsafe {
+            let pid = libc::fork();
+
+            if pid == -1 {
+                return Err("fork".into());
+            }
+
+            if pid == 0 {
+                let arg0 = CString::new(config.exe().as_bytes()).unwrap();
+                let args = vec![arg0.as_ptr(), std::ptr::null()];
+                libc::execvp(arg0.as_ptr(), args.as_ptr());
+                libc::exit(libc::EXIT_FAILURE);
+            }
+
+            Ok(Self::new(TaskInfo::new(config, pid)))
+        }
+    }
+
+    fn wait(self) -> TaskResult {
+        unsafe {
+            let status: i32 = 0;
+            libc::waitpid(self.info().pid(), status as *mut i32, 0);
+            let status = libc::WEXITSTATUS(status);
+            TaskResult::new(self.info().clone(), status)
+        }
     }
 }
